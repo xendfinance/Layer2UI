3af3359cd9cefa25aabb7656656adb3ef7631a61
franko4don <franko4don@yahoo.com>
2021-11-11 11:04:54 -0600

    added pm2

---
+++ parse-apps.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps : [{
+        name        : 'Layer2',
+        script      : 'npm start',
+        watch       : false,
+        merge_logs  : true,
+        cwd         : '/var/www/layer2',
+    }]
+};
