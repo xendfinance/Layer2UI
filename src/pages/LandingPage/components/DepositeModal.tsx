db9e3cd31fcb635c7445d9ad28b40cc7c7a5d298
Cally Nnamani <cartrf@live.com>
2021-11-29 13:19:12 +0100

    disable xvault deposit temporarily

--- src/pages/LandingPage/components/DepositeModal.tsx
+++ src/pages/LandingPage/components/DepositeModal.tsx
@@ -257,6 +257,7 @@ const DepositeModal: React.FC<Props> = ({ open, setOpen, balance, netAPY, vaulta
 
                             </Box>
                             {
+                                asset.protocolName.toLowerCase() !== 'xvault' &&
                                 Number(availableDeposite) > 0 &&
                                     Number(depositAmount) > 0 &&
                                     Number(depositAmount) <= Number(availableDeposite) ?
