bfc08c9c18ab2957354a84b883a94f02559ccdc5
kay albertus <kayalbertus@gmail.com>
2022-01-26 12:29:45 +0200

    Layer 2 Update With V2 Proxy Contracts

--- src/abiManager/index.ts
+++ src/abiManager/index.ts
@@ -5,14 +5,25 @@ import BUSD from './busd.json';
 import USDC from './usdc.json';
 import BNB from './bnb.json';
 import xvVaultUSDT from './xvVaultUsdtAbi.json';
+import xvVaultUSDTV2 from './V2XVault/XVaultUSDTV2.json';
 import xvVaultBUSD from './xvVaultBusdAbi.json';
+import xvVaultBUSDV2 from './V2XVault/XVaultBUSDV2.json';
 import xvVaultUSDC from './xvVaultUsdcAbi.json';
+import xvVaultUSDCV2 from './V2XVault/XVaultUSDCV2.json';
 
 //xAuto
 import xvAutoUSDT from './xvAutoUsdtAbi.json';
+import xvAutoBSCUSDTV2 from './V2XAuto/xvAutoUSDTV2.json';
+import xvAutoBSCBUSDV2 from './V2XAuto/xvAutoBUSDV2.json';
+import xvAutoBSCBNBV2 from './V2XAuto/xvAutoBNBV2.json';
+import xvAutoBSCUSDCV2 from './V2XAuto/xvAutoUSDCV2.json';
 import xvAutoUSDC from './xvAutoUsdcAbi.json';
+import xvAutoUSDCV2 from './V2XAutoMatic/xvAutoUSDCV2.json';
+import xvAutoUSDTV2 from './V2XAutoMatic/xvAutoUSDTV2.json';
 import xvAutoAAVE from './xvAutoAaveAbi.json';
+import xvAutoAAVEV2 from './V2XAutoMatic/xvAutoAAVEV2.json';
 import xvAutoWBTC from './xvAutoWbtcAbi.json';
+import xvAutoWBTCV2 from './V2XAutoMatic/xvAutoWBTCV2.json';
 import USDTMatic from './POSUsdtAbi.json';
 import USDCMatic from './POSUsdcAbi.json';
 import AAVEMatic from './POSAAVEAbi.json';
@@ -31,21 +42,32 @@ const abiManager = {
 	BUSD,
 	USDC,
 	xvVaultUSDT,
+	xvVaultUSDTV2,
 	xvVaultBUSD,
+	xvVaultBUSDV2,
 	xvVaultUSDC,
+	xvVaultUSDCV2,
 	USDTMatic,
 	xvAutoUSDT,
+	xvAutoUSDTV2,
 	USDCMatic,
 	AAVEMatic,
 	WBTCMatic,
 	xvAutoUSDC,
+	xvAutoUSDCV2,
 	xvAutoAAVE,
+	xvAutoAAVEV2,
 	xvAutoWBTC,
+	xvAutoWBTCV2,
 	APYPoolMatic,
 	xvAutoBSCBUSD,
+	xvAutoBSCBUSDV2,
 	xvAutoBSCUSDC,
+	xvAutoBSCUSDCV2,
 	xvAutoBSCUSDT,
+	xvAutoBSCUSDTV2,
 	xvAutoBSCBNB,
+	xvAutoBSCBNBV2,
 	BNB,
 	APYPoolBSC	
 }
