d66a67f68bbf241b6f124f4df523553a62e070f3
Franklin <franko4don@yahoo.com>
2023-10-13 17:07:36 +0100

    Update parse-apps.config.js

--- parse-apps.config.js
+++ parse-apps.config.js
@@ -4,6 +4,6 @@ module.exports = {
         script      : 'npm start',
         watch       : false,
         merge_logs  : true,
-        cwd         : '/var/www/layer2',
+        cwd         : '/var/www/autoyield.xend.finance',
     }]
 };
